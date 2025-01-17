16826e37d14bc78bed36995565f926eae30c3b3e
Tatiana Golub <tatiana.k1989@gmail.com>
2025-01-17 20:37:21 +0200

    feat: solve 1st task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,13 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-fs.readFile(
-  path.join(
-    __dirname,
-    'C:UsersТатьянаDesktopHTML-builder\x01-read-file\text.txt',
-  ),
-  'utf8',
-  (err, data) => {
-    if (err) return console.error(err);
-    console.log(data);
-  },
-);
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
+
+readStream.on('data', (chunk) => {
+  console.log(chunk.toString());
+});
